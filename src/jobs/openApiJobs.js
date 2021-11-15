597e18a1d51425c34bc4d74ecae68ba60ec34fd8
Donghoon Lee <cjswodlehdgn@gmail.com>
2021-11-15 22:46:02 +0900

    [feat]: 연구 목록 생성 API (#10)
    1. node-cron을 통해 주기적으로 OpenAPI fetch
    2. 받아온 데이터를 DB에 저장
    3. 해시값이 달라진 경우, 업데이트
    
    Co-authored-by: SibaDoge <38933716+SibaDoge1@users.noreply.github.com>

--- src/jobs/openApiJobs.js
+++ src/jobs/openApiJobs.js
@@ -7,52 +7,54 @@ const trialService = require('../services/trialService')
 
 const logger = require('../utils/logger.js');
 const logtag = 'src:batch';
-
+const { createOrUpdateTrials } = require('../services/trialService.js');
 
 function startOpenApiBatch() {
-  cron.schedule("*/10 * * * * *", () => {
+  cron.schedule('*/10 * * * * *', () => {
     getOpenApiData();
-  })
+  });
 }
 
 async function getOpenApiData() {
   let page = 1;
   let totalCount = 0;
-  let url='';
+  let url = '';
 
   while (1) {
     try {
-      url = "https://api.odcloud.kr/api/3074271/v1/uddi:cfc19dda-6f75-4c57-86a8-bb9c8b103887?page="+page+"&perPage=100&serviceKey=" + secretKey;
+      url =
+        'https://api.odcloud.kr/api/3074271/v1/uddi:cfc19dda-6f75-4c57-86a8-bb9c8b103887?page=' +
+        page +
+        '&perPage=100&serviceKey=' +
+        secretKey;
 
-      const res = await axios.get(url)
+      const res = await axios.get(url);
 
       // 더 이상 받아올게 없으면 종료
       totalCount += res.data.currentCount;
-      if(res.data.currentCount <= 0){
+      if (res.data.currentCount <= 0) {
         logger.logWithTag('total : ' + totalCount, logtag);
         break;
       }
 
       // 임상정보 오브젝트마다 해쉬값을 만들어서 넣어줌
       let trials = res.data.data;
-      trials.map((trial) => {
+      trials.map(trial => {
         let hash = md5(trial);
         trial.hash = hash;
         return trial;
       });
 
-      //DB 함수 호출
-      trialService.createOrUpdateTrials(trials);
+      await createOrUpdateTrials(trials);
 
       page++;
-      //logger.logWithTag(trials[0], logtag)
+      logger.logWithTag(trials[0], logtag);
     } catch (err) {
-      //logger.logWithTag(err, logtag)
+      logger.logWithTag(err, logtag);
       break;
     }
   }
 }
-
 
 module.exports.startOpenApiBatch = startOpenApiBatch;
 
--- src/services/trialService.js
+++ src/services/trialService.js
@@ -1,6 +1,9 @@
 const models = require('../models');
 const { InternalServerError } = require('http-errors');
-const { EntityNotExistError, ValidationError } = require('../utils/errors/commonError');
+const {
+  EntityNotExistError,
+  ValidationError,
+} = require('../utils/errors/commonError');
 const logger = require('../utils/logger');
 const trial = require('../models/trial');
 const { sequelize } = require('../models');
@@ -21,7 +24,7 @@ exports.readTrial = async (trialId)=>{
   }catch(err){
     throw err;
   }
-}
+};
 
 /**
  * 목록조회
@@ -39,7 +42,7 @@ exports.readTrialList = async (data)=>{
   }catch(err){
     throw err;
   }
-}
+};
 
 /**
  * 검색
@@ -68,7 +71,7 @@ exports.searchTrials = async (data)=>{
   }catch(err){
     throw err;
   }
-}
+};
 
 /**
  * 생성 혹은 변경
@@ -85,11 +88,43 @@ exports.searchTrials = async (data)=>{
  *           hash: '3449c9e5e332f1dbb81505cd739fbf3f'
  *        }
  */
-exports.createOrUpdateTrials= async (data)=>{
-  try{
+exports.createOrUpdateTrials = async data => {
+  try {
+    let newTrials = [];
 
-  }catch(err){
+    for (let i = 0; i < data.length; i++) {
+      let trial = data[i];
+
+      const dbRawData = {
+        id: trial['과제번호'],
+        name: trial['과제명'],
+        term: trial['연구기간'],
+        domain: trial['연구범위'],
+        type: trial['연구종류'],
+        host: trial['연구책임기관'],
+        model: trial['임상시험단계(연구모형)'],
+        subjectCount: trial['전체목표연구대상자수'],
+        department: trial['진료과'],
+        hash: trial['hash'],
+      };
+
+      //존재유무 확인
+      const alreadyTrial = await models.trial.findByPk(trial['과제번호']);
+
+      if (!alreadyTrial) newTrials.push(dbRawData);
+      //해쉬가 다르면 업데이트
+      else if (alreadyTrial.hash !== trial.hash) {
+        await models.trial.update(dbRawData, {
+          where: {
+            id: trial['과제번호'],
+          },
+        });
+      }
+    }
+
+    await models.trial.bulkCreate(newTrials);
+    return;
+  } catch (err) {
     throw err;
   }
-	
-}
+};
